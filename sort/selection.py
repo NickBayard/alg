4826ebd0ff2a0a393d48d0f4c5e76e7c0d405cf3
Nick Bayard <nicholas.bayard@netapp.com>
2020-09-04 23:00:28 +0000

    Adding selection sort

---
+++ sort/selection.py
@@ -0,0 +1,11 @@
+def selection_sort(data):
+    for index, current in enumerate(data):
+        least = index
+        for index2, check in enumerate(data[index:]):
+            if check < data[least]:
+                least = index2 + index
+        if least != index:
+            data[index], data[least] = data[least], data[index]
+
+    return data
+
