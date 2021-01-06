2ef71ca6b399ca728f23934a8c8414acc642c1b4
Nick Bayard <nbayard@gmail.com>
2021-01-05 22:35:15 -0700

    Red black tree 'put' method is done.

--- cpp/src/balanced_tree.cpp
+++ cpp/src/balanced_tree.cpp
@@ -1,39 +1,21 @@
 #include <memory>
 #include <cstdlib>
 
+enum class Color
+{
+    BLACK,
+    RED,
+};
+
 class Node
 {
 private:
-    enum class Color
-    {
-        BLACK,
-        RED,
-    };
 
     int key;
     Color color;
 
     Node* left{nullptr};
     Node* right{nullptr};
-
-    Node* rotateLeft()
-    {
-        Node* temp = right;
-        right = temp->left;
-        temp->left = this;
-        temp->color = color;
-        color = Color::RED;
-        return temp;
-    }
-
-    Node* rotateRight()
-    {
-        return this;
-    }
-
-    void flipColors()
-    {
-    }
 
 public:
     Node(int key, Color color=Color::BLACK): key{key}, color{color} {}
@@ -44,10 +26,93 @@ public:
         delete right;
     }
 
+    friend Node * rotateLeft(Node *);
+    friend Node * rotateRight(Node *);
+    friend void flipColors(Node *);
+    friend Node * correct(Node *);
+
     void put(int key)
     {
+        if (key < this->key)
+        {
+            if (!left)
+            {
+                left = new Node(key, Color::RED);
+            }
+            else
+            {
+                left->put(key);
+            }
+        }
+        else if (key > this->key)
+        {
+            if (!right)
+            {
+                right = new Node(key, Color::RED);
+            }
+            else
+            {
+                right->put(key);
+            }
+        }
+
+        if (right)
+        {
+            right = correct(right);
+        }
+        if (left)
+        {
+            left = correct(left);
+        }
     }
 };
+
+Node * rotateLeft(Node * node)
+{
+    Node * temp = node->right;
+    node->right = temp->left;
+    temp->left = node;
+    temp->color = node->color;
+    node->color = Color::RED;
+    return temp;
+}
+
+Node * rotateRight(Node * node)
+{
+    Node * temp = node->left;
+    node->left = temp->right;
+    temp->right = node;
+    temp->color = node->color;
+    node->color = Color::RED;
+    return temp;
+}
+
+void flipColors(Node * node)
+{
+    node->color = Color::RED;
+    node->left->color = Color::BLACK;
+    node->right->color = Color::BLACK;
+}
+
+Node * correct(Node * node)
+{
+    if ((node->left && node->left->color == Color::BLACK) &&
+        (node->right && node->right->color == Color::RED))
+    {
+        node = rotateLeft(node);
+    }
+    if (node->left && node->left->color == Color::RED &&
+        node->left->left && node->left->left->color == Color::RED)
+    {
+        node = rotateRight(node);
+    }
+    if (node->left && node->left->color == Color::RED &&
+        node->right && node->right->color == Color::RED)
+    {
+        flipColors(node);
+    }
+    return node;
+}
 
 int main()
 {
